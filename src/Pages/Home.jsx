6bbc9a8124cd80ed6dcc51436843b069bb89deb2
unknown <mehedytouhidurzaman@gmail.com>
2024-08-30 00:10:53 +0600

    Header complted

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import Header from './Shared/Header';
+import React from 'react'
+import Header from './Shared/Header'
 import Navbar from './Shared/Navbar'
+import LeftSideNavbar from './Shared/LeftSideNavbar'
+import RightSideNavbar from './Shared/RightSideNavbar'
 
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <Navbar></Navbar>
-            <h1 className='font-poppins'>This is Home page</h1>
+            <div className='grid md:grid-cols-4'>
+                <div>
+                    <LeftSideNavbar></LeftSideNavbar>
+                </div>
+                <div className='md:col-span-2'>
+                    <h1>This is the news component</h1>
+                </div>
+                <div>
+                    <RightSideNavbar></RightSideNavbar>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Header.jsx
+++ src/Pages/Shared/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import logo from '../../assets/logo.png'
+import moment from "moment";
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+      <div className='flex flex-col justify-center items-center space-y-5'>
+        <img src={logo} alt="" />
+        <p>Journalism Without Fear or Favour</p>
+        <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+      </div>
     );
 };
 
