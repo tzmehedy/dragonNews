2acb7041110de6dfe94bd9efb45cc2a34d92e647
unknown <mehedytouhidurzaman@gmail.com>
2024-08-30 23:29:47 +0600

    left side nav desing completed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,16 +9,16 @@ const Home = () => {
     return (
       <div>
         <Header></Header>
-        <div className="bg-gray-400 px-3 py-2 rounded-lg flex mt-5">
+        <div className="bg-gray-200 px-3 py-2 rounded-lg flex mt-5">
           <button className="btn btn-secondary px-8">Latest</button>
-          <Marquee className="font-bold text-pink-600" pauseOnHover={true}>
+          <Marquee className="font-bold text-gray-800" pauseOnHover={true}>
             Match Highlights: Germany vs Spain — as it happened ! Match
             Highlights: Germany vs Spain as...
           </Marquee>
         </div>
         <Navbar></Navbar>
 
-        <div className="grid md:grid-cols-4">
+        <div className="grid md:grid-cols-4 mt-20">
           <div>
             <LeftSideNavbar></LeftSideNavbar>
           </div>
--- src/Pages/Shared/RightSideNavbar.jsx
+++ src/Pages/Shared/RightSideNavbar.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
+
+import qZone1 from '../../assets/qZone1.png'
+import qZone2 from '../../assets/qZone2.png'
+import qZone3 from '../../assets/qZone3.png'
+
+import bg1 from '../../assets/bg.png'
 
 const RightSideNavbar = () => {
+
+    const bg = `bg-[url("${bg1}")]`;
+
+    console.log(bg)
     return (
+      <div>
+        {/* Login with google and facebook */}
         <div>
-            <h1>This is right side Navbar</h1>
+          <h1 className="text-xl font-bold">Login With</h1>
+          <div className="flex flex-col ">
+            <button className="btn btn-outline mb-3 mt-2 ">
+              {" "}
+              <FaGoogle className="text-yellow-800"></FaGoogle> Login With
+              Google
+            </button>
+            <button className="btn btn-outline">
+              {" "}
+              <FaGithub className="text-black"></FaGithub> Login With Github
+            </button>
+          </div>
         </div>
+
+        {/* Find with us */}
+
+        <div className="mt-10">
+          <h1 className="text-xl font-bold">Find Us On</h1>
+          <div className="mt-5">
+            <a
+              className="flex items-center gap-2 border border-gray-600 px-3 py-2 rounded-t-lg"
+              href=""
+            >
+              <FaFacebook className="text-blue-800"></FaFacebook> Facebook
+            </a>
+            <a
+              className="flex items-center gap-2 border border-y-0 border-gray-600 px-3 py-2"
+              href=""
+            >
+              <FaTwitter className="text-sky-800"></FaTwitter> Twitter
+            </a>
+            <a
+              className="flex items-center gap-2 border border-gray-800 px-3 py-2 rounded-b-lg"
+              href=""
+            >
+              <FaInstagram className="text-pink-800"></FaInstagram> Instagram
+            </a>
+          </div>
+        </div>
+
+        {/* Q-zone */}
+
+        <div>
+          <h1 className="text-xl font-bold mt-10">Q-Zone</h1>
+          <div>
+            <img src={qZone1} alt="" />
+          </div>
+          <div>
+            <img src={qZone2} alt="" />
+          </div>
+          <div>
+            <img src={qZone3} alt="" />
+          </div>
+        </div>
+
+        {/* Ad */}
+
+        <div className="bg-cover bg-no-repeat p-20 text-white space-y-5" style={{ backgroundImage: `url(${bg1})`}}>
+          <h1 className='font-bold text-xl'>Create an Amazing Newspaper</h1>
+          <p>
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <button className='btn btn-secondary'>Learn More</button>
+        </div>
+      </div>
     );
 };
 
